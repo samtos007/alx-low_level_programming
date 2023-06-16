51eb8ecabddbdb576017c182ff50e790d2831a13
samtos007 <tobibamigbademobax@gmail.com>
2023-06-16 11:50:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function of the program
+ *
+ * Return: This is always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
