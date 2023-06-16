0717fd6591dc207b32a0df6af2fbcd8035b863d6
samtos007 <tobibamigbademobax@gmail.com>
2023-06-16 05:56:02 -0700

    r was uppercased R in the description for return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 
 /**
  * main - function of the program and it prints using puts function
- * Description: 'This prints an output with puts'
- * return: Will Always be 0
+ *
+ * Return: Will Always 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
