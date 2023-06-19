9135b175fa45b4f4ca4744f84079f9d454dde952
samtos007 <tobibamigbademobax@gmail.com>
2023-06-19 13:13:12 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - primary function
+ *
+ * description - prints all possible combination of single digits
+ *
+ * Return: should be 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	a = 48;
+	b = 44;
+	c = 32;
+	while (a < 58)
+	{
+		putchar(a);
+		putchar(b);
+		putchar(c);
+		a++;
+	}
+	putchar(10);
+
+	return (0);
+}
