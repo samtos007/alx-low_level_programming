c50a8e074e948abd818c981d053cc9a208817ee3
samtos007 <tobibamigbademobax@gmail.com>
2023-06-27 07:50:27 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - the functin tkes a pointer to an integer parameter
+ *
+ * @n: the integer pointer
+ *
+ * Return: Always 0 if successful
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
