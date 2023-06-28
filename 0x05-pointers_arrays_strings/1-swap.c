d1d99597c9ee8a1e4e47c7f0e487318b5911c6e4
samtos007 <tobibamigbademobax@gmail.com>
2023-06-27 19:06:47 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * main - Primary function and swaps values
- *
- * @swap_int: swap function
- *
- * Return: Always 0 for a successful run.
- */
+* swap_int - The function that swaps
+*
+* @a: first integer
+*
+* @b: second integer
+*
+* Return: Always 0 for a successful run.
+*/
 
 void swap_int(int *a, int *b)
 {
-	int unit = *a;
-	*a = *b;
-        *b = unit;
-}       
+int unit = *a;
+*a = *b;
+*b = unit;
+}
