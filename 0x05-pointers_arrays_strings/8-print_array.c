b2c28ab713d50e0d8dc00f8fbeb3888d1ab60b06
samtos007 <tobibamigbademobax@gmail.com>
2023-06-28 10:49:26 -0700

    New commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,15 +7,19 @@
  * description - The program prints the 'n' elements of an array
  *
  * @n: coun of the number of input digits
- * @a:
+ * @a: input array
  *
  * Return: returns nothing or void
  */
 void print_array(int *a, int n)
 {
-	for (int i = 0; i < n; i++)
+	int i = 0;
+
+	for (0; i < n; i++)
 	{
-		printf("%d ", a[i]);
+		printf("%d ", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
 	}
 	printf("\n");
 }
