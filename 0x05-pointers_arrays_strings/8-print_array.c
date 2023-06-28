6f30759e9b1bf783418e3599c09ccb587af232b7
samtos007 <tobibamigbademobax@gmail.com>
2023-06-28 11:09:24 -0700

    Commit after successful test with 8-main.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -17,7 +18,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; ++i)
 	{
 		if (i != (n - 1))
-			print("%d, ", a[i]);
+			printf("%d, ", a[i]);
 		else
 			printf("%d", a[i]);
 	}
