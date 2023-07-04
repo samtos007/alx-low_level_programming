62da4281bd112b8999f675e0be1a2ddc878d46a0
samtos007 <tobibamigbademobax@gmail.com>
2023-07-04 08:53:33 -0700

    updated code

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @s: input string.
- * Return: the pointer 
+ * Return: the pointer
  */
 
 char *string_toupper(char *s)
