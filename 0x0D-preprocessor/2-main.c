a475d1da6e652f2c55563b39a6dcd9120d8f6096
samtos007 <tobibamigbademobax@gmail.com>
2023-07-17 16:15:26 -0700

    main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - primary function that prints the nma eof the file
+ *
+ * Return: Always 0 if successful
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
